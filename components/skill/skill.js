243569612a181945e9bf5ef13723504800a2a3d5
Janne Parviainen <janne.j.parviainen@gmail.com>
2020-06-12 23:25:43 +0300

    lazy icon load

--- components/skill/skill.js
+++ components/skill/skill.js
@@ -5,7 +5,7 @@ const ProfessionalSkill = (skillContent) => {
   const { alternativeText, skill } = skillContent;
   return (
     <SkillColumn>
-      <Img src={`https:${skillContent.icon.fields.file.url}`} alt={alternativeText} />
+      <Img src={`https:${skillContent.icon.fields.file.url}`} alt={alternativeText} loading="lazy" />
       <Skill>{skill}</Skill>
     </SkillColumn>
   );
